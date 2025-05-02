819dd54eaeee5aefcd528e3f83c4eb6f2afcfa2d
João Luís <jolitp@gmail.com>
2025-05-01 22:31:17 -0300

    configures CLI tools

--- sh.nix
+++ sh.nix
@@ -7,12 +7,19 @@ let
     #la = "ls -a";
     #lla = "ls -la";
 
+    # cd -> broot
+    #cd = "broot";
+
     # ls -> eza
     ls = "eza --group-directories-first --icons ";
     lt = "eza --group-directories-first --icons --tree";
     ll = "eza --group-directories-first --icons --long";
     la = "eza --group-directories-first --icons --all";
+    lta = "eza --group-directories-first --icons --tree --all";
     lla = "eza --group-directories-first --icons --long --all";
+
+    # cat -> bat
+    cat = "bat";
 
     # Directory traversal
     ".." = "cd ..";
@@ -88,83 +95,7 @@ in
 #    };
     programs.starship = {
       enable = true;
-#      settings = pkgs.lib.importTOML;
-#      settings = {
-#        add_newline = true;
-#        command_timeout = 1300;
-#        scan_timeout = 50;
-
-        # example
-#        format = "$all$nix_shell$nodejs$lua$golang$rust$php$git_branch$git_commit$git_state$git_status\n$username$hostname$directory";
-
-	# tolyo night
-#        format = "[░▒▓](#a3aed2)[ 󱄅 ](bg:#a3aed2 fg:#090c0c)[▓▒░](bg:#769ff0 fg:#a3aed2)$directory[░▒▓](fg:#769ff0 bg:#394260)$git_branch$git_status[▓▒░](fg:#394260 bg:#212736)$nodejs$rust$golang$php[░▒▓](fg:#212736 bg:#1d2230)$time[▓▒░](fg:#1d2230)$character";
-#
-#	directory = {
-#          style = "fg:#e3e5e5 bg:#769ff0";
-#          format = "[ $path ]($style)";
-#          truncation_length = 3;
-#          truncation_symbol = "…/";
-#        };
-#        
-#        directory.substitutions = {
-#          Documents = "󱧶 ";
-#          "Downloads" = "󰉍 ";
-#          "Music" = "󱍙 ";
-#          "Pictures" = "󰉏 ";
-#        };
-#        
-#        git_branch = {
-#          symbol = "";
-#          style = "bg:#394260";
-#          format = "[[ $symbol $branch ](fg:#769ff0 bg:#394260)]($style)";
-#	};
-#        
-#        git_status = {
-#          style = "bg:#394260";
-#          format = "[[($all_status$ahead_behind )](fg:#769ff0 bg:#394260)]($style)";
-#	};
-#        
-#        nodejs = {
-#          symbol = "";
-#          style = "bg:#212736";
-#          format = "[[ $symbol ($version) ](fg:#769ff0 bg:#212736)]($style)";
-#	};
-#        
-#        rust = {
-#          symbol = "";
-#          style = "bg:#212736";
-#          format = "[[ $symbol ($version) ](fg:#769ff0 bg:#212736)]($style)";
-#	};
-#        
-#        golang = {
-#          symbol = "";
-#          style = "bg:#212736";
-#          format = "[[ $symbol ($version) ](fg:#769ff0 bg:#212736)]($style)";
-#	};
-#        
-#        php = {
-#          symbol = "";
-#          style = "bg:#212736";
-#          format = "[[ $symbol ($version) ](fg:#769ff0 bg:#212736)]($style)";
-#	};
-#        
-#        time = {
-#          disabled = false;
-#          time_format = "%R"; # Hour:Minute Format
-#          style = "bg:#1d2230";
-#          format = "[[  $time ](fg:#a0a9cb bg:#1d2230)]($style)";
-#	};
-
-        # pastel
-#       format = "[](#9A348E)$os$username[](bg:#DA627D fg:#9A348E)$directory[](fg:#DA627D bg:#FCA17D)$git_branch$git_status[](fg:#FCA17D bg:#86BBD8)$c$elixir$elm$golang$gradle$haskell$java$julia$nodejs$nim$rust$scala[](fg:#86BBD8 bg:#06969A)$docker_context[](fg:#06969A bg:#33658A)$time[ ](fg:#33658A)";
         settings =  pkgs.lib.importTOML ./home/config/starship/tokyo-night.toml;
-
-#        character = {
-#          success_symbol = "[](bold green) ";
-#          error_symbol = "[](bold red) ";
-#        }; # character
-#      }; # settings
     }; # programs.starship
 
 #    programs.atuin = {
@@ -175,14 +106,43 @@ in
       enable = true;
       #icons = "always";
 
-      #extraOptions = [
-      #  "--tree"
-      #  "--icons"
-      #]; # extraOptions
-
       git = true;
 
     }; # programs.eza
+
+    programs.bat = {
+      enable = true;
+      config = {
+        pager = "less -FR";
+        theme = "Nord";
+      };
+    }; # programs.bat
+
+    programs.broot = {
+      enable = true;
+      settings = {
+        ext-colors = {
+          nix = "cyan";
+	};
+      };
+    }; #programs.broot
+
+    programs.fd.enable = true;
+    programs.yazi.enable = true;
+    programs.ripgrep.enable = true;
+
+    programs.zoxide = {
+      enable = true;
+      enableBashIntegration = true;
+    };
+
+    programs.zellij = {
+      enable = true;
+      enableBashIntegration = true;
+      settings = {
+        theme = "nord";
+      };
+    };
 
   }; # config
 }
