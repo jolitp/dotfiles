54c00538d5204662134a0673ac6bd86bff9e8b35
jolitp <jolitp@gmail.com>
2025-05-09 17:19:39 -0300

    moves firefox to web browsers module

--- home/home.nix
+++ home/home.nix
@@ -5,6 +5,7 @@
   imports = [
     ./modules/sh.nix
     ./modules/cli.nix
+    ./modules/web_browsers.nix
   ]; # imports
 
   config = {
@@ -290,7 +291,6 @@
       };
     }; # programs.git
 
-
     programs.nh = {
       enable = true;
       #clean.enable = true;
@@ -310,82 +310,6 @@
         };
       }; # settings
     }; # programs.alacritty
-
-    programs.firefox = {
-      enable = true;
-      
-      profiles.jolitp = {
-        # bookmarks sync is done using floccus browser extension & NextCloud bookmarks
-        #https://floccus.org/
-
-        #bookmarks.configfile = ./config/firefox/firefox-bookmarks.html;
-        #bookmarks = [
-        #  {
-              #     name = "MyNisOS";
-        #     url = "https://mynixos.com/";
-        #  }
-        #]; # bookmarks = [
-
-        settings = {
-          # ... look them up
-        };
-            
-        search.engines = {
-          "Nix Packages" = {
-            urls = [
-              {
-                  template = "https://search.nixos.org/packages";
-                  params = [
-                    { name = "type"; value = "packages"; }
-                    { name = "query"; value = "{searchTerms}"; }
-                  ]; # params
-                  icon = "${pkgs.nixos-icons}/share/icons/hicolor/scalable/apps/nix-snowflake.svg";
-                  definedAliases = [ "@np" ];
-              }
-            ]; # urls
-          }; # "Nix Packages"
-        }; # search.engines
-        search.force = true;
-        
-        userChrome = # CSS to change the browser theme
-        ''  
-        ''; # userChrome = # CSS to change the browser theme
-
-        #extensions = with inputs.firefox-addons.packages."x86_64-linux"; [
-        extensions = with pkgs.nur.repos.rycee.firefox-addons; [
-          darkreader
-          ublock-origin
-          languagetool
-          #enhancer-for-nebula
-          noscript
-          privacy-badger
-          # share-backported # not found
-          sidebery
-          single-file
-          stylebot-web
-          tranquility-1 # tranquility reader
-          translate-web-pages
-          # yet another speeddial # not found
-          enhancer-for-youtube
-          youtube-high-definition
-          downthemall
-          markdownload
-          # whatsapp redirect # not found
-          # whatsend # not found
-          canvasblocker
-          # duplicate tabs closer # not found
-          # hide youtube fullscreen controls # not found
-          # medium parser # not found 
-          # path of exile trade - fuzzy search # not found
-          sponsorblock # youtube sponsorblock
-          user-agent-string-switcher # should be the same (without the "string")
-          vimium-c
-          # web signer (softplan) # not found
-          floccus
-          # auto-highlight # not found
-        ]; # extensions
-      }; # profiles.jolitp = {
-    }; # programs.firefox = {
 
     programs.zellij = {
       enable = true;
