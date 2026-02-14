02e56690cf7a852282a493d179e431d154aafc15
jolitp <jolitp@gmail.com>
2026-02-14 02:00:39 -0300

    moves nh to it's own module

--- hosts/_modules/nh.nix
+++ hosts/_modules/nh.nix
@@ -1,3 +1,4 @@
+{ config, pkgs, pkgs-unstable, lib, inputs, userSettings, ... }:
 
 {
   config = {
@@ -8,8 +9,10 @@
       # needs to be set in the nixos module
       # does not work in the home-manager module
       # it errors out with null
-      #clean.enable = true;
-      #clean.extraArgs = "--keep-since 7d"; 
+      clean.enable = true;
+      clean.extraArgs = "--keep-since 7d"; 
+      clean.dates = "weekly";
+
     }; # programs.nh
-  } # config = {
+  }; # config = {
 }
