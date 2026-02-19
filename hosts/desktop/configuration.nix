a6c7e29a24c6031466283afe379b74394e3f49c8
jolitp <jolitp@gmail.com>
2026-02-19 05:09:35 -0300

    adds sound module to system

--- hosts/desktop/configuration.nix
+++ hosts/desktop/configuration.nix
@@ -9,12 +9,23 @@
     # Include the results of the hardware scan.
     ./hardware-configuration.nix
     ./gpu.nix
+    ../packages.nix
+
+    ../_modules/sound.nix
+    ../_modules/bootloader.nix
+    ../_modules/virtualization.nix
+    ../_modules/sh.nix
+    ../_modules/kde.nix
+    ../_modules/xdg.nix
+
+    ../_modules/flatpak.nix
+    ../_modules/nh.nix
   ];
 
   # Bootloader.
-  boot.loader.grub.enable = true;
-  boot.loader.grub.device = "/dev/sda";
-  boot.loader.grub.useOSProber = true;
+  # boot.loader.grub.enable = true;
+  # boot.loader.grub.device = "/dev/sda";
+  # boot.loader.grub.useOSProber = true;
 
   networking.hostName = "desktop"; # Define your hostname.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
@@ -49,8 +60,8 @@
   services.xserver.enable = true;
 
   # Enable the KDE Plasma Desktop Environment.
-  services.displayManager.sddm.enable = true;
-  services.desktopManager.plasma6.enable = true;
+  # services.displayManager.sddm.enable = true;
+  # services.desktopManager.plasma6.enable = true;
 
   # Configure keymap in X11
   services.xserver.xkb = {
@@ -63,22 +74,6 @@
 
   # Enable CUPS to print documents.
   services.printing.enable = true;
-
-  # Enable sound with pipewire.
-  services.pulseaudio.enable = false;
-  security.rtkit.enable = true;
-  services.pipewire = {
-    enable = true;
-    alsa.enable = true;
-    alsa.support32Bit = true;
-    pulse.enable = true;
-    # If you want to use JACK applications, uncomment this
-    #jack.enable = true;
-
-    # use the example session manager (no others are packaged yet so this is enabled by default,
-    # no need to redefine it in your config for now)
-    #media-session.enable = true;
-  };
 
   # Enable touchpad support (enabled default in most desktopManager).
   # services.xserver.libinput.enable = true;
