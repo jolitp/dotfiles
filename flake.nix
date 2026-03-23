cc02cde4d98cfa5363d76956a8a87b9a68c35206
jolitp <jolitp@gmail.com>
2026-03-23 17:24:13 -0300

    formatting

--- flake.nix
+++ flake.nix
@@ -27,135 +27,140 @@
     catppuccin.url = "github:catppuccin/nix/release-25.11";
   }; # inputs
 
-  outputs = {
-    self,
-    nixpkgs,
-    nixpkgs-unstable,
-    home-manager,
-    stylix,
-    catppuccin,
-    #nvf,
-    ...
-  } @ inputs: let
-    # ---- SYSTEM SETTINGS ---- #
-    systemSettings = {
-      system_architecture = "x86_64-linux"; # system architecture
-      hostname = "laptop"; # hostname
-      #profile = "personal"; # select a profile defined from my profiles directory
-      timezone = "America/Sao_Paulo"; # select timezone
-      locale = "en_US.UTF-8"; # select locale
-      sytem_shell = "bash"; # bash or zsh
-      #bootMode = "uefi"; # uefi or bios
-      #bootMountPath = "/boot"; # mount path for efi boot partition; only used for uefi boot mode
-      #grubDevice = ""; # device identifier for grub; only used for legacy (bios) boot mode
-      #gpuType = "amd"; # amd, intel or nvidia; only makes some slight mods for amd at the moment
-    };
+  outputs =
+    {
+      self,
+      nixpkgs,
+      nixpkgs-unstable,
+      home-manager,
+      stylix,
+      catppuccin,
+      #nvf,
+      ...
+    }@inputs:
+    let
+      # ---- SYSTEM SETTINGS ---- #
+      systemSettings = {
+        system_architecture = "x86_64-linux"; # system architecture
+        hostname = "laptop"; # hostname
+        #profile = "personal"; # select a profile defined from my profiles directory
+        timezone = "America/Sao_Paulo"; # select timezone
+        locale = "en_US.UTF-8"; # select locale
+        sytem_shell = "bash"; # bash or zsh
+        #bootMode = "uefi"; # uefi or bios
+        #bootMountPath = "/boot"; # mount path for efi boot partition; only used for uefi boot mode
+        #grubDevice = ""; # device identifier for grub; only used for legacy (bios) boot mode
+        #gpuType = "amd"; # amd, intel or nvidia; only makes some slight mods for amd at the moment
+      };
 
-    # ----- USER SETTINGS ----- #
-    userSettings = rec {
-      username = "jolitp"; # username
-      name = "João Luís"; # name/identifier
-      email = "jolitp@gmail.com"; # email (used for certain configurations)
-      dotfilesDir = "~/dotfiles"; # absolute path of the local repo
-      theme = "tokyonight"; # selcted theme from my themes directory (./themes/)
-      #wm = "hyprland"; # Selected window manager or desktop environment;
-      # must select one in both ./user/wm/ and ./system/wm/
-      # window manager type (hyprland or x11) translator
-      #wmType = if ((wm == "hyprland") || (wm == "plasma")) then "wayland" else "x11";
-      #browser = "qutebrowser"; # Default browser; must select one from ./user/app/browser/
-      #spawnBrowser = if ((browser == "qutebrowser") && (wm == "hyprland")) then "qutebrowser-hyprprofile" else (if (browser == "qutebrowser") then "qutebrowser --qt-flag enable-gpu-rasterization --qt-flag enable-native-gpu-memory-buffers --qt-flag num-raster-threads=4" else browser); # Browser spawn command must be specail for qb, since it doesn't gpu accelerate by default (why?)
-      #defaultRoamDir = "Personal.p"; # Default org roam directory relative to ~/Org
-      terminal = "alacritty"; # Default terminal command;
-      font = "Intel One Mono"; # Selected font
-      nerdFont = "IntoneMono Nerd Font";
-      fontPkg = pkgs.intel-one-mono; # Font package
-      editor = "nvim"; # Default editor;
-    };
+      # ----- USER SETTINGS ----- #
+      userSettings = rec {
+        username = "jolitp"; # username
+        name = "João Luís"; # name/identifier
+        email = "jolitp@gmail.com"; # email (used for certain configurations)
+        dotfilesDir = "~/dotfiles"; # absolute path of the local repo
+        theme = "tokyonight"; # selcted theme from my themes directory (./themes/)
+        #wm = "hyprland"; # Selected window manager or desktop environment;
+        # must select one in both ./user/wm/ and ./system/wm/
+        # window manager type (hyprland or x11) translator
+        #wmType = if ((wm == "hyprland") || (wm == "plasma")) then "wayland" else "x11";
+        #browser = "qutebrowser"; # Default browser; must select one from ./user/app/browser/
+        #spawnBrowser = if ((browser == "qutebrowser") && (wm == "hyprland")) then "qutebrowser-hyprprofile" else (if (browser == "qutebrowser") then "qutebrowser --qt-flag enable-gpu-rasterization --qt-flag enable-native-gpu-memory-buffers --qt-flag num-raster-threads=4" else browser); # Browser spawn command must be specail for qb, since it doesn't gpu accelerate by default (why?)
+        #defaultRoamDir = "Personal.p"; # Default org roam directory relative to ~/Org
+        terminal = "alacritty"; # Default terminal command;
+        font = "Intel One Mono"; # Selected font
+        nerdFont = "IntoneMono Nerd Font";
+        fontPkg = pkgs.intel-one-mono; # Font package
+        editor = "nvim"; # Default editor;
+      };
 
-    lib = nixpkgs.lib;
-    system = "x86_64-linux";
-    #pkgs = nixpkgs.legacyPackages.${system};
-    pkgs-unstable = nixpkgs-unstable.legacyPackages.${system};
+      lib = nixpkgs.lib;
+      system = "x86_64-linux";
+      #pkgs = nixpkgs.legacyPackages.${system};
+      pkgs-unstable = nixpkgs-unstable.legacyPackages.${system};
 
-    pkgs = import nixpkgs {
-      inherit system;
-      #legacyPackages.${system};
-      config = {allowUnfree = true;};
-    };
-  in {
-    # System Configurations
-    nixosConfigurations = {
-      desktop = lib.nixosSystem {
+      pkgs = import nixpkgs {
         inherit system;
-        modules = [
-          # ./hosts/${systemSettings.hostname}/configuration.nix
-          ./hosts/desktop/configuration.nix
-          stylix.nixosModules.stylix
-          catppuccin.nixosModules.catppuccin
-        ];
-        specialArgs = {
+        #legacyPackages.${system};
+        config = {
+          allowUnfree = true;
+        };
+      };
+    in
+    {
+      # System Configurations
+      nixosConfigurations = {
+        desktop = lib.nixosSystem {
           inherit system;
-          #inherit pkgs;
-          inherit pkgs-unstable;
-          inherit systemSettings;
-          inherit userSettings;
-        };
-      }; # desktop
+          modules = [
+            # ./hosts/${systemSettings.hostname}/configuration.nix
+            ./hosts/desktop/configuration.nix
+            stylix.nixosModules.stylix
+            catppuccin.nixosModules.catppuccin
+          ];
+          specialArgs = {
+            inherit system;
+            #inherit pkgs;
+            inherit pkgs-unstable;
+            inherit systemSettings;
+            inherit userSettings;
+          };
+        }; # desktop
 
-      laptop = lib.nixosSystem {
-        inherit system;
-        modules = [
-          # ./hosts/${systemSettings.hostname}/configuration.nix
-          ./hosts/laptop/configuration.nix
-          stylix.nixosModules.stylix
-          catppuccin.nixosModules.catppuccin
-        ];
-        specialArgs = {
+        laptop = lib.nixosSystem {
           inherit system;
-          #inherit pkgs;
-          inherit pkgs-unstable;
-          inherit systemSettings;
-          inherit userSettings;
-        };
-      }; # desktop
+          modules = [
+            # ./hosts/${systemSettings.hostname}/configuration.nix
+            ./hosts/laptop/configuration.nix
+            stylix.nixosModules.stylix
+            catppuccin.nixosModules.catppuccin
+          ];
+          specialArgs = {
+            inherit system;
+            #inherit pkgs;
+            inherit pkgs-unstable;
+            inherit systemSettings;
+            inherit userSettings;
+          };
+        }; # desktop
 
-      vm = lib.nixosSystem {
-        inherit system;
-        modules = [
-          # ./hosts/${systemSettings.hostname}/configuration.nix
-          ./hosts/vm/configuration.nix
-          stylix.nixosModules.stylix
-          catppuccin.nixosModules.catppuccin
-        ];
-        specialArgs = {
+        vm = lib.nixosSystem {
           inherit system;
-          #inherit pkgs;
-          inherit pkgs-unstable;
-          inherit systemSettings;
-          inherit userSettings;
-        };
-      }; # vm
-    }; # nixosConfigurations
+          modules = [
+            # ./hosts/${systemSettings.hostname}/configuration.nix
+            ./hosts/vm/configuration.nix
+            stylix.nixosModules.stylix
+            catppuccin.nixosModules.catppuccin
+          ];
+          specialArgs = {
+            inherit system;
+            #inherit pkgs;
+            inherit pkgs-unstable;
+            inherit systemSettings;
+            inherit userSettings;
+          };
+        }; # vm
+      }; # nixosConfigurations
 
-    # Home Manager Configurations
-    homeConfigurations = {
-      # jolitp user's config
-      jolitp = home-manager.lib.homeManagerConfiguration {
-        inherit pkgs;
-        #inherit pkgs-unstable; # error
+      # Home Manager Configurations
+      homeConfigurations = {
+        # jolitp user's config
+        jolitp = home-manager.lib.homeManagerConfiguration {
+          inherit pkgs;
+          #inherit pkgs-unstable; # error
 
-        modules = [
-          ./home/home.nix
-          catppuccin.homeModules.catppuccin
-        ]; # modules
+          modules = [
+            ./home/home.nix
+            catppuccin.homeModules.catppuccin
+          ]; # modules
 
-        extraSpecialArgs = {
-          #inherit pkgs;
-          inherit pkgs-unstable;
-          inherit inputs;
-          inherit userSettings;
-        };
-      }; # jolitp = home-manager.lib.homeManagerConfiguration
-    }; # homeConfigurations = {
-  }; # outputs
+          extraSpecialArgs = {
+            #inherit pkgs;
+            inherit pkgs-unstable;
+            inherit inputs;
+            inherit userSettings;
+          };
+        }; # jolitp = home-manager.lib.homeManagerConfiguration
+      }; # homeConfigurations = {
+    }; # outputs
 }
